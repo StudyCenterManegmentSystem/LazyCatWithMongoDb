3f007b3fed0c57d23b8c3da2545017a5bbf1daf4
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-23 10:10:59 +0500

    biroz o'zgarish qildim

--- Application/Dtos/TeacherDto/TeacherRegisterRequest.cs
+++ Application/Dtos/TeacherDto/TeacherRegisterRequest.cs
@@ -1,35 +1,34 @@
 ﻿using Domain.Entities.Entity.Teachers;
 using System.ComponentModel.DataAnnotations;
 
-namespace Application.Dtos.TeacherDto
+namespace Application.Dtos.TeacherDto;
+
+public class TeacherRegisterRequest
 {
-    public class TeacherRegisterRequest
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password), Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+
+    public List<string>? FanIds { get; set; }
+
+    public static implicit operator TeacherReturnDto(TeacherRegisterRequest teacherRequest)
     {
-        [Required, EmailAddress]
-        public string Email { get; set; } = string.Empty;
-
-        [Required, StringLength(100)]
-        public string FirstName { get; set; } = string.Empty;
-
-        [Required, StringLength(100)]
-        public string LastName { get; set; } = string.Empty;
-
-        [Required, DataType(DataType.Password)]
-        public string Password { get; set; } = string.Empty;
-
-        [Required, DataType(DataType.Password), Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
-        public string ConfirmPassword { get; set; } = string.Empty;
-
-        public List<string>? FanIds { get; set; }
-
-        public static implicit operator TeacherDto(TeacherRegisterRequest teacherRequest)
+        return new TeacherReturnDto
         {
-            return new Domain.Entities.Entity.Teachers.Teacher
-            {
-                Email = teacherRequest.Email,
-                FirstName = teacherRequest.FirstName,
-                LastName = teacherRequest.LastName,
-            };
-        }
+            Email = teacherRequest.Email,
+            FirstName = teacherRequest.FirstName,
+            LastName = teacherRequest.LastName,
+        };
     }
 }
