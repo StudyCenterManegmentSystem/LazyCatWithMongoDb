a7d4d6e4c87470f8d9b24c2a7763cec4a459dbda
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-26 15:49:32 +0500

    Rasm yuklash uchun project ochib uni yozdim ammo haqo ishlayapti

---
+++ StudyCenterManegmentSystem.StorageService/Services/IS3Interface.cs
@@ -0,0 +1,8 @@
+﻿namespace StudyCenterManegmentSystem.StorageService.Services;
+
+public interface IS3Interface
+{
+    public Task<string> UploadFileAsync(IFormFile file);
+    public Task<Stream> GetFileUrlAsync(string fileName);
+    public Task DeleteFileAsync(string fileName);
+}
