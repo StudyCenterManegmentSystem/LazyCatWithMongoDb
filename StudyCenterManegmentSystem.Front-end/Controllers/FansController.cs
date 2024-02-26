8dfad7728568681a19741407602fb8d08d9d5825
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-26 18:32:47 +0500

    Frontend ni boshladik

---
+++ StudyCenterManegmentSystem.Front-end/Controllers/FansController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dtos.FanDtos;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyCenterManegmentSystem.Front_end.Controllers;
+
+public class FansController(IFanService fanService) : Controller
+{
+    public readonly IFanService _fanService = fanService;
+
+    public async Task<IActionResult> Index() 
+    {
+        var fans = await _fanService.GetAllAsync(); 
+        var returnFans = fans.Select(f => (FanDto)f).ToList(); 
+        return View(returnFans); 
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    public IActionResult Save(AddFanDto dto)
+    {
+
+        _fanService.AddAsync(dto);
+        return RedirectToAction("Index");
+    }
+}
+
+
+
