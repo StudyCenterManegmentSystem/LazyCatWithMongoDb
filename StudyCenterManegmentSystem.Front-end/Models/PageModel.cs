cc2e95838da2c7feb57317c8e0df5470e72d261e
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-26 21:09:38 +0500

    PageModel

--- StudyCenterManegmentSystem.Front-end/Models/PageModel.cs
+++ StudyCenterManegmentSystem.Front-end/Models/PageModel.cs
@@ -1,23 +1,22 @@
-﻿namespace StudyCenterManegmentSystem.Front_end.Models
+﻿namespace StudyCenterManegmentSystem.Front_end.Models;
+
+public class PageModel<T>
 {
-    public class PageModel<T>
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalItemsCount { get; set; }
+    public List<T> Items { get; set; } = new List<T>();
+
+    public PageModel(List<T> items, int pageNumber, int pageSize = 5)
     {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-        public int TotalPages { get; set; }
-        public int TotalItemsCount { get; set; }
-        public List<T> Items { get; set; } = new List<T>();
-
-        public PageModel(List<T> items, int pageNumber, int pageSize = 5)
-        {
-            PageSize = pageSize;
-            PageNumber = pageNumber;
-            TotalItemsCount = items.Count;
-            TotalPages = (int)Math.Ceiling(TotalItemsCount / (double)pageSize);
-            Items = items.Skip(pageSize * (pageNumber - 1))
-                            .Take(pageSize)
-                            .ToList();
-        }
-
+        PageSize = pageSize;
+        PageNumber = pageNumber;
+        TotalItemsCount = items.Count;
+        TotalPages = (int)Math.Ceiling(TotalItemsCount / (double)pageSize);
+        Items = items.Skip(pageSize * (pageNumber - 1))
+                        .Take(pageSize)
+                        .ToList();
     }
+
 }
