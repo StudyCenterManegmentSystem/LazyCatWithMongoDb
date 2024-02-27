2489a2b2f5d4157875bb5c02ef73b395b75b55d5
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-27 23:49:03 +0500

    AttendanceDtos ha tugadi

---
+++ Application/Dtos/AttendanceDtos/AttendanceReturnDto.cs
@@ -0,0 +1,12 @@
+﻿using Application.Dtos.GroupsDto;
+using Application.Dtos.StudentDtos;
+
+namespace Application.Dtos.AttendanceDtos;
+
+public class AttendanceReturnDto : BaseDto
+{
+    public StudentReturnDto? Student { get; set; } 
+    public DateTime Qachon { get; set; }
+    public bool KeldiKemadi { get; set; } = true;
+    public GuruhReturnDto? Guruh { get; set; }
+}
---
+++ Application/Dtos/AttendanceDtos/UpdateAttendanceDto.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities.Entity.Attendances;
+
+namespace Application.Dtos.AttendanceDtos;
+
+public class UpdateAttendanceDto : BaseDto
+{
+    public int TalabaId { get; set; }
+    public DateTime Qachon { get; set; }
+    public bool KeldiKemadi { get; set; } = true;
+    public string? GroupId { get; set; }
+
+    public static implicit operator UpdateAttendanceDto(Attendance attendance)
+        => new UpdateAttendanceDto()
+        {
+            TalabaId = attendance.TalabaId,
+            GroupId = attendance.GroupId,
+            Qachon = attendance.Qachon,
+            KeldiKemadi = attendance.KeldiKemadi
+        };
+    public static implicit operator Attendance(UpdateAttendanceDto dto)
+        => new Attendance()
+        {
+            TalabaId = dto.TalabaId,
+            GroupId = dto.GroupId,
+            Qachon = dto.Qachon,
+            KeldiKemadi = dto.KeldiKemadi
+        };
+}
