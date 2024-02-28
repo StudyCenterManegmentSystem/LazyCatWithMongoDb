7e946bded12b1908c415531320a546d15bdd427f
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-28 19:25:42 +0500

    .

--- Web/Program.cs
+++ Web/Program.cs
@@ -2,15 +2,12 @@ using Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Configure services
 Startup.ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseAuthorization();
 
 Startup.Configure(app, app.Environment);
