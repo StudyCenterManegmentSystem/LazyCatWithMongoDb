84a0c268d1236511d50b80f0b169952fd3aba9b3
kamoliddin <kamoliddinzaylobiddinov677@gmail.com>
2024-03-01 00:58:37 +0500

    Guruh boshlandi

---
+++ CRM.Admin/Models/GuruhDtos/Guruh.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRM.Admin.Models.GuruhDtos;
+
+public class Guruh
+{
+    public string Id { get; set; } = string.Empty;
+    public string GroupName { get; set; } = string.Empty;
+    public string? RoomId { get; set; }
+    public string? FanId { get; set; }
+    public string? TeacherId { get; set; }
+    public List<int>? Weekdays { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public decimal Price { get; set; }
+    public string? Duration { get; set; }
+}
